c69dd95d6f8d2101af6b77456972c63087780087
Gopher Kaseya <20kw174@esisalama.org>
2024-02-06 16:18:36 +0100

    mise en forme boostrap

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-    if($_POST['nom_ville']){
+    if(isset($_POST['nom_ville'])
+    ){
         $url = "https://api.openweathermap.org/data/2.5/weather?q=paris&appid=60ba69b1b1304b3143174510fd4797c5";
 
         $raw = file_get_contents($url);
@@ -14,21 +15,33 @@
     
     
 ?>
-<!DOCTYPE html>
+
+<!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Meteo</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-    <form action="" method="post">
-        <input type="text" name="nom_ville" id="">
-        <button>Valider</button>
-    </form>
-    <?php
-        if($temp)
-            echo "la temperature est de ". $temp;
-    ?>
+    <div class="container mt-3">
+        <form action="" class="form-group d-flex " method="post">
+            <div class="">
+                <input class="form-control" type="text" name="nom_ville" id="">
+            </div>
+            <div>
+                <button class="btn btn-primary">Valider</button>
+            </div>
+        </form>
+        
+        <div>
+            <?php
+            if(isset($temp))
+                echo "la temperature est de ". $temp;cd
+            ?>
+        </div>
+    </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
